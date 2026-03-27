13db7e982a6cb9621fe47ae931edc1d432566b8c
dannapina262718-tech <danna.pina262718@potros.itson.edu.mx>
2026-03-27 14:50:14 -0700

    ClienteDAO documentado y con pruebas

--- Persistencia/src/main/java/daos/ClienteDAO.java
+++ Persistencia/src/main/java/daos/ClienteDAO.java
@@ -16,9 +16,9 @@ public class ClienteDAO {
      * @return cliente guardado
      */
     public Cliente guardar(Cliente cliente){
-        //creamos la conexion a la BD
+        
         EntityManager em = ConexionBD.crearConexion();
-        //
+        
         try {
             em.getTransaction().begin();
             em.persist(cliente); //guardamos cliente
@@ -28,16 +28,21 @@ public class ClienteDAO {
         } catch (Exception e) {
             if (em.getTransaction().isActive()) {
                 em.getTransaction().rollback();
-                //rollback en caso de que algo falle
+                
             }
             throw e;
         }finally{
-            em.close(); //cerramos la conexion
+            em.close(); 
         }
     }
     
+    /**
+     * buscar un cliente por nombre
+     * @param nom nombre a buscar en los reguitros
+     * @return en caso de encontrara coincidencias un cliente en caso contrario null
+     */
     public List<Cliente> buscarPorNombre(String nom){
-        //creamos la conexion a la BD
+        
         EntityManager em = ConexionBD.crearConexion();
         try {
             List<Cliente> clientes = em.createQuery("SELECT c FROM Cliente c WHERE c.nombres = :nombre", Cliente.class)
@@ -49,8 +54,13 @@ public class ClienteDAO {
         }
     }
     
+    /**
+     * buscar un cliente por telefono
+     * @param tel telefono a buscar en los reguistros
+     * @return en caso de encontrara coincidencias un cliente en caso contrario null
+     */
     public List<Cliente> buscarPorTelefono(String tel){
-        //creamos la conexion a la BD
+        
         EntityManager em = ConexionBD.crearConexion();
         try {
             List<Cliente> clientes = em.createQuery("SELECT c FROM Cliente c WHERE c.telefono = :telefono", Cliente.class)
@@ -62,8 +72,14 @@ public class ClienteDAO {
         }
     }
     
+    
+    /**
+     * buscar un cliente por correo
+     * @param corr correo a buscar en los reguistros
+     * @return en caso de encontrara coincidencias un cliente en caso contrario null
+     */
     public List<Cliente> buscarPorCorreo(String corr){
-        //creamos la conexion a la BD
+        
         EntityManager em = ConexionBD.crearConexion();
         try {
             List<Cliente> clientes = em.createQuery("SELECT c FROM Cliente c WHERE c.correo = :correo", Cliente.class)
---
+++ Persistencia/src/test/java/daos/ClienteDAOTest.java
@@ -0,0 +1,85 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package daos;
+
+import entidades.Cliente;
+import java.time.LocalDate;
+import java.util.List;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author piña
+ */
+public class ClienteDAOTest {
+
+    //Preparacion
+    private ClienteDAO instance;
+
+    public ClienteDAOTest() {
+    }
+
+    @BeforeEach
+    public void setUp() {
+        instance = new ClienteDAO();
+    }
+    //Test de cada metodo
+
+    @Test
+    public void testGuardar() {
+        System.out.println("Ejecutando: guardar");
+
+        // Creamos un cliente 
+        String correoPrueba = "danna@mail.com";
+        Cliente cliente = new Cliente("Danna", "Ibarra", "Piña", "6441224266", correoPrueba, LocalDate.now());
+        Cliente result = instance.guardar(cliente);
+
+        // Verificaciones (Assertions)
+        assertNotNull(result, "El cliente guardado no debería ser nulo");
+        assertNotNull(result.getId(), "La base de datos debería haber generado un ID");
+        assertEquals("Danna", result.getNombres());
+    }
+
+    @Test
+    public void testBuscarPorNombre() {
+        System.out.println("Prueba Individual: buscarPorNombre");
+        String nom = "Danna"; // Nombre que ya existe en la tabñla de clientes
+
+        List<Cliente> result = instance.buscarPorNombre(nom);
+
+        assertNotNull(result);
+        assertFalse(result.isEmpty(), "Debería encontrar al menos un registro con ese nombre");
+    }
+
+    @Test
+    public void testBuscarPorTelefono() {
+        System.out.println("Prueba Individual: buscarPorTelefono");
+        String tel = "6441224266";
+
+        List<Cliente> result = instance.buscarPorTelefono(tel);
+
+        assertNotNull(result);
+        // Verificamos que el teléfono del primer resultado sea el correcto
+        assertEquals(tel, result.get(0).getTelefono());
+    }
+
+    @Test
+    public void testBuscarPorCorreo() {
+        System.out.println("Prueba Individual: buscarPorCorreo");
+        String corr = "danna@mail.com";
+
+        List<Cliente> result = instance.buscarPorCorreo(corr);
+
+        assertNotNull(result);
+        // Verificamos que el correo del primer resultado sea el correcto
+        assertEquals(corr, result.get(0).getCorreo());
+    }
+
+}
