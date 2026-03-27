dfe716d6060ee19dc41a5e081d1690561eeae55c
dannapina262718-tech <danna.pina262718@potros.itson.edu.mx>
2026-03-27 12:55:15 -0700

    clienteDAO pt1

---
+++ Persistencia/src/main/java/daos/ClienteDAO.java
@@ -0,0 +1,52 @@
+
+package daos;
+import conexion.ConexionBD;
+import entidades.Cliente;
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.EntityManager;
+/**
+ *
+ * @author piña
+ */
+public class ClienteDAO {
+    
+    /**
+     * guardar un cliente en la BD
+     * @param cliente a guardad
+     * @return cliente guardado
+     */
+    public Cliente guardar(Cliente cliente){
+        //creamos la conexion a la BD
+        EntityManager em = ConexionBD.crearConexion();
+        //
+        try {
+            em.getTransaction().begin();
+            em.persist(cliente); //guardamos cliente
+            em.getTransaction().commit(); 
+            
+            return cliente;
+        } catch (Exception e) {
+            if (em.getTransaction().isActive()) {
+                em.getTransaction().rollback();
+                //rollback en caso de que algo falle
+            }
+            throw e;
+        }finally{
+            em.close(); //cerramos la conexion
+        }
+    }
+    
+    public List<Cliente> buscarPorNombre(String nom){
+        //creamos la conexion a la BD
+        EntityManager em = ConexionBD.crearConexion();
+        try {
+            List<Cliente> clientes = em.createQuery("SELECT c FROM Cliente c WHERE c.nombres = :nombre", Cliente.class)
+                           .setParameter("nombre", nom)
+                           .getResultList();
+            return clientes;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+}
